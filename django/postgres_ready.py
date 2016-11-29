9b06127df8891a78ab8ca71ea5935072239a6b46
Eric Rasche <rasche.eric@gmail.com>
2016-11-29 20:38:19 +0000

    Log error

--- django/postgres_ready.py
+++ django/postgres_ready.py
@@ -3,6 +3,7 @@ import sys
 import psycopg2
 try:
     conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host=sys.argv[1])
-except psycopg2.OperationalError:
+except psycopg2.OperationalError, e:
+    print e
     sys.exit(-1)
 sys.exit(0)
